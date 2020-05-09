ba434bc1036563e031b029b4fae3ebb4c4b9bbd9
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2020-05-09 16:31:04 -0300

    feat: criado projeto e tela de cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
+import { SignUpComponent } from './sign-up/sign-up.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'signup', pathMatch: 'full' },
+  { path: '*', redirectTo: 'signup', pathMatch: 'full' },
+
+  { path: 'signup', component: SignUpComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
