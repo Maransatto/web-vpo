9b978e35f6b1043b2e8661c4cf79915e64489739
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2020-05-12 07:37:51 -0300

    feat(sidebar.component): organizada a ordem dos links do sidebar

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+  nomeContexto = "Nome do Contexto";
+
   constructor() { }
 
   ngOnInit() {
